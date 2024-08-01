618cfad307cc240c9f19835ecd5373ae82ae7c15
johnson-713 <johnsonelangovan@gmail.com>
2024-08-01 16:22:50 +0530

    optimizing data

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,25 +1,5 @@
-const sidebarData = [
-    {
-        id: "55vjhvhj",
-        img: "/icons/home-white.svg",
-      },
-      {
-        id: "55vjhvytdt",
-        img: "/icons/chart.svg",
-      },
-      {
-        id: "55vjhtydtfg",
-        img: "/icons/clipboard.svg",
-      },
-      {
-        id: "55vjhguvhj",
-        img: "/icons/wallet.svg",
-      },
-      {
-        id: "55vjhvgcgytdt",
-        img: "/icons/bag-ok.svg",
-      },
-]
+import { sidebarData } from "../constants/constants"
+
 
 export const Sidebar = () => {
   return (
---
+++ src/constants/constants.js
@@ -0,0 +1,37 @@
+export const sidebarData = [
+    {
+        id: "55vjhvhj",
+        img: "/icons/home-white.svg",
+      },
+      {
+        id: "55vjhvytdt",
+        img: "/icons/chart.svg",
+      },
+      {
+        id: "55vjhtydtfg",
+        img: "/icons/clipboard.svg",
+      },
+      {
+        id: "55vjhguvhj",
+        img: "/icons/wallet.svg",
+      },
+      {
+        id: "55vjhvgcgytdt",
+        img: "/icons/bag-ok.svg",
+      },
+]
+
+export const navData = [
+    {
+      id: "55vjhvhj",
+      img: "/icons/mail.svg",
+    },
+    {
+      id: "55vjhvytdt",
+      img: "/icons/setting.svg",
+    },
+    {
+      id: "55vjhtydtfg",
+      img: "/icons/notification.svg",
+    },
+  ];
